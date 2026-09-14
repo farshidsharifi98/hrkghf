6546eebbcce1ac621bf5ce2d619d2b9d652eb4c5
Farshid <farshidsharifi3@gmail.com>
2026-09-13 21:12:40 -0800

    https://github.com/farshidsharifi98/hrkghf.git

--- f/urls.py
+++ f/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from f.views import http_test,json_test
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('http-test', http_test),
+    path('json-test',json_test)
 ]
