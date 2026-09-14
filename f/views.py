9989816c68f8aae96ae21444a893a47b2e781aa0
Farshid <farshidsharifi3@gmail.com>
2026-09-13 21:48:22 -0800

    https://github.com/farshidsharifi98/hrkghf.git

--- f/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-
-def http_test(request):
-    return HttpResponse(t'http_test')
-
-def json_test(request):
-    return JsonResponse({'name':'mohammad'})
